c9114c763236f34509442cbe1e074d75d4e44b8c
raulize <62007805+raulize@users.noreply.github.com>
2020-03-27 17:30:05 +0900

    Done
    header / data requests
    
    to do
    
    make a table

---
+++ homework.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+
+
+data = requests.get("https://www.genie.co.kr/chart/top200?ditc=D&rtm=N&ymd=20200309",
+
+                    headers=headers
+
+                    )
+
