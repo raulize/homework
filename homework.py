cfe52040ee6748a68ff07918671b66e2e51e48c2
raulize <62007805+raulize@users.noreply.github.com>
2020-03-31 17:15:34 +0900

    Done
    header / data requests
    
    to do
    
    delete 상승/하강, new 키워드

--- homework.py
+++ homework.py
@@ -1,12 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
+headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'}
+data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&rtm=N&ymd=20200309',
 
-headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-
-
-data = requests.get("https://www.genie.co.kr/chart/top200?ditc=D&rtm=N&ymd=20200309",
-
-                    headers=headers
+                    headers = headers
 
                     )
+
+soup = BeautifulSoup(data.text, 'html.parser')
+list = soup.select('#body-content > div.newest-list > div > table > tbody > tr')
+
+for lank in list:
+
+    lankNum = lank.select_one('td.number')
+    title = lank.select_one('a.title.ellipsis')
+    artist = lank.select_one('a.artist.ellipsis')
+
+    if title is not None:
+
+        print(lankNum.text.lstrip(),"/",artist.text.lstrip(),"/",title.text.lstrip())
+
 
